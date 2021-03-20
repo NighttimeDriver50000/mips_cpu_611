e22c22dc264d527214a1aa28ac62b53667af8877
Embers McKinney <em@embermckinney.com>
2021-03-20 19:53:20 -0400

    Python 3 compat

--- tool/padprogram.sh
+++ tool/padprogram.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd "$(dirname "$0")/.."
-python -c "print '\n'.join(['00000000']*(1024-($(wc -l program_hex.txt | cut -d' ' -f1))))" \
+python -c "print('\n'.join(['00000000']*(1024-($(wc -l program_hex.txt | cut -d' ' -f1)))))" \
     >> program_hex.txt
-python -c "print '\n'.join(['00000000']*(1024-($(wc -l data_hex.txt | cut -d' ' -f1))))" \
+python -c "print('\n'.join(['00000000']*(1024-($(wc -l data_hex.txt | cut -d' ' -f1)))))" \
     >> data_hex.txt
